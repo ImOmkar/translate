7ce8a8732c51b2a03a889c1b84c3e196928113b4
ImOmkar <omkar211196@gmail.com>
2022-09-30 22:50:33 +0530

    python-dotenv added

--- translate/settings.py
+++ translate/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 from decouple import config
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY") 
+#SECRET_KEY = config("SECRET_KEY") 
+#my_secret_key = os.getenv("SECRET_KEY")
+load_dotenv()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
