cd95024b41ad07afcc273bbf73657b0010719aa9
ImOmkar <omkar211196@gmail.com>
2022-09-30 21:54:09 +0530

    gunicorn added for ebs

--- translate/settings.py
+++ translate/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
 
-ALLOWED_HOSTS = ['django-env.eba-p38d3bpq.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-env.eba-p38d3bpq.us-west-2.elasticbeanstalk.com', '*']
 
 
 # Application definition
