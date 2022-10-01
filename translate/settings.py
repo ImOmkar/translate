cf0a68854925126b3fae7c4a7366d96db76b1442
ImOmkar <omkar211196@gmail.com>
2022-10-01 12:27:49 +0530

    staticfiles storage updated

--- translate/settings.py
+++ translate/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]  
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # new
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # new
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 # Default primary key field type
