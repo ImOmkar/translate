ac433a26b168faeac2dcab35b558441773945296
ImOmkar <omkar211196@gmail.com>
2022-10-02 23:20:51 +0530

    textonimage added

--- translate/urls.py
+++ translate/urls.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path('devToModi/clearcache/', include('clearcache.urls')),
     path('devToModi/', admin.site.urls),
