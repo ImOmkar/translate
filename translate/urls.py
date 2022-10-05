3aff07c2db07ba2de98f84ca60d10c5422c4e9fc
ImOmkar <omkar211196@gmail.com>
2022-10-05 22:49:23 +0530

    pwabuilder files added

--- translate/urls.py
+++ translate/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.urls import path, include
 
 urlpatterns = [
     path('devToModi/clearcache/', include('clearcache.urls')),
     path('devToModi/', admin.site.urls),
     path('', include('modi.urls')),
+
+    #pwabuilder.js link
+    path('pwabuilder-sw.js', TemplateView.as_view(template_name="static/pwabuilder-sw.js", content_type="application/javascript"), name="pwabuilder-sw.js"),
 
     
 ] 
