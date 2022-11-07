348ed57c920805ff38e5d750136666caf4cdcf5a
ImOmkar <omkar211196@gmail.com>
2022-11-07 10:06:18 +0530

    removed unused packages

--- translate/settings.py
+++ translate/settings.py
@@ -34,14 +34,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-
     "whitenoise.runserver_nostatic",
-
     #'django.contrib.staticfiles',    
-
     'modi',
-    'widget_tweaks',
-
     'clearcache',
 ]
 
