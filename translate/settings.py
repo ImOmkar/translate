449f73f5d9faff6a26285a69131de2fc396bdeea
ImOmkar <omkar211196@gmail.com>
2024-05-04 23:32:46 +0530

    google tag added to analytics

--- translate/settings.py
+++ translate/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = os.environ.get('DEBUG')     
 
-SECRET_KEY = True #os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = ['*']
 
